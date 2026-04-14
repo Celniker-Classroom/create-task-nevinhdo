7a1febd38073f9d12b01de73044bf3b4a4254c7a
nevinhdo <don0909@my.sduhsd.net>
2026-04-14 20:26:52 +0000

    started workng on the actual game and creating the randomized list and stuff

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // add javascript here
+
+const wordlist = ["apple", "horizon", "whisper", "glitch", "mountain", "velvet", "thunder", "pocket", "cascade", "nebula", "rhythm", "bridge", "silver", "anchor", "jungle", "frozen", "locket", "echo", "button", "puddle", "galaxy", "window", "crimson", "marble", "breeze", "clover", "starlight", "cactus", "tunnel", "puzzle", "orbit", "fable", "drifting", "mellow", "quartz", "vivid", "shadow", "prairie", "copper", "journey", "mitten", "harbor", "zenith", "cricket", "sketch", "legend", "pioneer", "flame", "ocean", "willow"];
+
+function getRandomWord() {
+    const randomIndex = Math.floor(Math.random() * wordlist.length);
+    return wordlist[randomIndex];
+    var randomWord = getRandomWord();
+    console.log(randomWord);
+}
+
+document.getElementById("randomword").textContent = getRandomWord();
+document.getElementById("randomWordButton").addEventListener("click", revealWord);
+
+
+function revealWord() {
+  var x = document.getElementById("randomword").style;
+  if (x.display === "none") {
+    x.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
