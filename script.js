9cf1b5431a76511a8516d94fb50f8d88e5ca2a99
nevinhdo <don0909@my.sduhsd.net>
2026-04-20 06:32:16 +0000

    I finished my game. I will check for all requirments next

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
-// add javascript here
-
-let correct = 0;
+//setting variables
 let randQuestion = "";
+let questionCount = 0;
+let userAnswers = [];
 
-const questionlist = ["What is 15 + 27?", "What is 84 - 39?", "What is 12 \ times 6?", "What is 144 / 12?", "What is 53 + 68?", "What is 120 - 45?", "What is 9 \ times 7?", "What is 56 / 8?", "What is 250 + 175?", "What is 15 \ times 4?"];
+//questions and answers lists
+const questionlist = ["What is 2 + 3?", "What is 5 + 4?", "What is 10 - 3?", "What is 8 - 2?", "What is 3 x 4?", "What is 5/1?", "What is 15/3?", "What is 2 x 4?", "What is 25/5?", "What is 9 - 8?"];
+const answerlist = ["5", "9", "7", "6", "12", "5", "5", "8", "5", "1"];
 
-const answerlist = ["42", "45", "72", "12", "121", "75", "63", "7", "425", "60"];
-
-function getQuestion() {
-    const randomIndex = Math.floor(Math.random() * questionlist.length);
-    return questionlist[randomIndex];
-}
-
+//Loading question function
 function loadQuestion() {
-    randQuestion = getQuestion();
-    document.getElementById("questionText").innerText = randQuestion;
-}
-
-function getOutput() {
-    const answer = document.getElementById("myInput").value;
-    return answer;
-}
-
-function checkAnswer(userAnswer) {
-    const questionIndex = questionlist.indexOf(randQuestion);
-    const correctAnswer = answerlist[questionIndex];
-    if (userAnswer === correctAnswer) {
-        correct++;
+    if (questionCount < 5) {
+        randQuestion = questionlist[questionCount];
+        document.getElementById("questionText").innerText = randQuestion;
     }
-    document.getElementById("score").innerText = "Score: " + correct;
-    document.getElementById("myInput").value = "";
-    loadQuestion();
 }
 
+//Checking answer function
+function checkAnswer(userAnswer) {
+    userAnswers.push(userAnswer);
+    questionCount++;
+    if (questionCount >= 5) {
+        const finalScore = checkAll(userAnswers);
+        document.getElementById("score").innerText = "Final Score: " + (finalScore) + "/5";
+        document.getElementById("questionText").innerText = "Quiz Complete!";
+        document.getElementById("myInput").disabled = true;
+        document.getElementById("myButton").disabled = true;
+    } else {
+        document.getElementById("score").innerText = "Questions answered: " + questionCount + "/5";
+        document.getElementById("myInput").value = "";
+        loadQuestion();
+    }
+}
+
+//Function to check all answers
+function checkAll(answers) {
+    let correct = 0;
+    for (let i = 0; i < answers.length; i++) {
+        if (answers[i] === answerlist[i]) {
+            correct++;
+        }
+    }
+    return correct;
+}
+
+//calling the load question function and setting up event listener for the submit button
 loadQuestion();
 document.getElementById("guessButton").addEventListener("submit", function(event) {
     event.preventDefault();
-    checkAnswer(getOutput());
+    checkAnswer(document.getElementById("myInput").value);
 });
-
