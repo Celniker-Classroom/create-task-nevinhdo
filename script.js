bf84f324650cad73e75216ad50c361da31d5b249
nevinhdo <don0909@my.sduhsd.net>
2026-04-20 05:44:35 +0000

    Finish working on quiz game. maybe make some changes now

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // add javascript here
 
 let correct = 0;
-let questions = 0;
+let randQuestion = "";
 
 const questionlist = ["What is 15 + 27?", "What is 84 - 39?", "What is 12 \ times 6?", "What is 144 / 12?", "What is 53 + 68?", "What is 120 - 45?", "What is 9 \ times 7?", "What is 56 / 8?", "What is 250 + 175?", "What is 15 \ times 4?"];
 
@@ -12,11 +12,9 @@ function getQuestion() {
     return questionlist[randomIndex];
 }
 
-const randQuestion = getQuestion();
-const questionElement = document.getElementById("questionText");
-if (questionElement) {
-    questionElement.innerText = randQuestion;
-    console.log(randQuestion);
+function loadQuestion() {
+    randQuestion = getQuestion();
+    document.getElementById("questionText").innerText = randQuestion;
 }
 
 function getOutput() {
@@ -24,15 +22,20 @@ function getOutput() {
     return answer;
 }
 
-function checkAnswer() {
-    const userAnswer = getOutput();
+function checkAnswer(userAnswer) {
     const questionIndex = questionlist.indexOf(randQuestion);
     const correctAnswer = answerlist[questionIndex];
     if (userAnswer === correctAnswer) {
         correct++;
-        questions++;
-    } else {
-        questions++;
     }
+    document.getElementById("score").innerText = "Score: " + correct;
+    document.getElementById("myInput").value = "";
+    loadQuestion();
 }
+
+loadQuestion();
+document.getElementById("guessButton").addEventListener("submit", function(event) {
+    event.preventDefault();
+    checkAnswer(getOutput());
+});
 
