efe64c04d266499cc3ab8da5cf605971df11491b
nevinhdo <don0909@my.sduhsd.net>
2026-04-20 15:07:14 +0000

    Fix some html and css

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-//setting variables
+
 let randQuestion = "";
 let questionCount = 0;
 let userAnswers = [];
 
-//questions and answers lists
+
 const questionlist = ["What is 2 + 3?", "What is 5 + 4?", "What is 10 - 3?", "What is 8 - 2?", "What is 3 x 4?", "What is 5/1?", "What is 15/3?", "What is 2 x 4?", "What is 25/5?", "What is 9 - 8?"];
 const answerlist = ["5", "9", "7", "6", "12", "5", "5", "8", "5", "1"];
 
-//Loading question function
+
 function loadQuestion() {
     if (questionCount < 5) {
         randQuestion = questionlist[questionCount];
@@ -15,7 +15,7 @@ function loadQuestion() {
     }
 }
 
-//Checking answer function
+
 function checkAnswer(userAnswer) {
     userAnswers.push(userAnswer);
     questionCount++;
@@ -32,7 +32,6 @@ function checkAnswer(userAnswer) {
     }
 }
 
-//Function to check all answers
 function checkAll(answers) {
     let correct = 0;
     for (let i = 0; i < answers.length; i++) {
@@ -43,7 +42,7 @@ function checkAll(answers) {
     return correct;
 }
 
-//calling the load question function and setting up event listener for the submit button
+
 loadQuestion();
 document.getElementById("guessButton").addEventListener("submit", function(event) {
     event.preventDefault();
