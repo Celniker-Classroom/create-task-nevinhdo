9a7a9cc609af3837f200570355bc0e5358cb8f54
nevinhdo <don0909@my.sduhsd.net>
2026-04-23 20:51:38 +0000

    had to restart because I broke it

--- script.js
+++ script.js
@@ -6,11 +6,36 @@ let correspondingAnswer = [];
 //creating lists
 const questionlist = ["What is 2 + 3?", "What is 5 + 4?", "What is 10 - 3?", "What is 8 - 2?", "What is 3 x 4?", "What is 5/1?", "What is 15/3?", "What is 2 x 4?", "What is 25/5?", "What is 9 - 8?"];
 const answerlist = ["5", "9", "7", "6", "12", "5", "5", "8", "5", "1"];
+let usedQuestions = [];
+let gameStage = "beginning";
+
+
+
+if (gameStage === "beginning") {
+        document.getElementById("myInput").disabled = true;
+        document.getElementById("myButton").disabled = true;
+        document.getElementById("startButton").addEventListener("click", function() {   
+        gameStage = "playing";
+        document.getElementById("myInput").disabled = false;
+        document.getElementById("myButton").disabled = false;
+            loadQuestion();
+        }
+    );
+}
+
+
+
+
+
 
 //loading questions
 function loadQuestion() {
     if (questionCount < 5) {
         randQuestion = questionlist[Math.floor(Math.random() * questionlist.length)];
+        while (usedQuestions.includes(randQuestion)) {
+            randQuestion = questionlist[Math.floor(Math.random() * questionlist.length)];
+        }
+        usedQuestions.push(randQuestion);
         correspondingAnswer.push(randQuestion);
         document.getElementById("questionText").innerText = randQuestion;
     }
